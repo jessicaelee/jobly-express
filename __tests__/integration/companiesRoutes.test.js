b874c30fa15012e6b54ce49918331c2bed6d584b
sairina <sairina.merinotsui@gmail.com>
2020-02-19 14:14:54 -0800

    finished get one handle route, patch route, tests for those routes

--- __tests__/integration/companiesRoutes.test.js
+++ __tests__/integration/companiesRoutes.test.js
@@ -7,27 +7,133 @@ const Company = require('../../models/company');
 
 describe("Company Routes Testing", function () {
 
-    let company1;
-    let company2;
+  let company1;
+  let company2;
 
-    beforeEach(async function () {
-        await db.query("DELETE FROM companies")
+  beforeEach(async function () {
+    await db.query("DELETE FROM companies")
 
-        company1 = await Company.create({ "handle": "comp1", "name": "testcompany1", "num_employees": 1000 })
-        company2 = await Company.create({ "handle": "comp2", "name": "testcompany2", "num_employees": 2000 })
+    company1 = await Company.create(
+      {
+        "handle": "comp1",
+        "name": "testcompany1",
+        "num_employees": 1000
+      });
+
+    company2 = await Company.create(
+      {
+        "handle": "comp2",
+        "name": "testcompany2",
+        "num_employees": 2000
+      });
+  });
+
+  describe("GET /companies", function () {
+    test("can get list of all companies", async function () {
+      let resp = await request(app).get('/companies');
+
+      expect(resp.statusCode).toBe(200);
+      expect(resp.body).toEqual({
+        'companies':
+          [{
+            "handle": company1.handle,
+            "name": company1.name
+          },
+          {
+            "handle": company2.handle,
+            "name": company2.name
+          }]
+      });
     });
 
-    describe("GET /companies", function () {
-        test("can get list of all companies", async function () {
-            let resp = await request(app).get('/companies');
+    test("will not get list of companies", async function () {
+      let resp = await request(app)
+        .get('/companies?min_employees=10&max_employees=5');
 
-            expect(resp.statusCode).toBe(200);
-            expect(resp.body).toEqual({ 'companies': [{ "handle": company1.handle, "name": company1.name }, { "handle": company2.handle, "name": company2.name }] })
-        })
+      expect(resp.statusCode).toBe(400);
+    });
+  });
+
+  describe("POST /companies", function () {
+    test("adds new company", async function () {
+      let newCompany = {
+        "handle": "nike",
+        "name": "Nike",
+        "num_employees": 150000,
+        "description": "company",
+        "logo_url": "hi"
+      };
+
+      let resp = await request(app)
+        .post('/companies')
+        .send(newCompany);
+
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body).toEqual({ 'company': newCompany });
+    });
+
+    test("doesn't add new company", async function () {
+      let failCompany = {
+        "name": "Nike",
+        "num_employees": 150000,
+        "description": "company",
+        "logo_url": "hi"
+      };
+
+      let resp = await request(app)
+        .post('/companies')
+        .send(failCompany);
+
+      expect(resp.statusCode).toBe(400);
     })
+  });
 
-    afterAll(function () {
-        db.end();
+  describe("GET /companies/:handle", function () {
+    test("can get company by handle", async function () {
+      let resp = await request(app)
+        .get(`/companies/${company1.handle}`);
+
+      expect(resp.statusCode).toBe(200);
+      expect(resp.body).toEqual({ 'company': company1 });
     });
+
+    test("will not get company that does not exist", async function () {
+      let resp = await request(app)
+        .get(`/companies/none-here`);
+
+      expect(resp.statusCode).toBe(404);
+    });
+  });
+
+  describe("PATCH /companies/:handle", function () {
+    test("can update company by handle", async function () {
+      let resp = await request(app)
+        .patch(`/companies/${company1.handle}`)
+        .send({ "name": "COMPANY!" });
+
+      expect(resp.statusCode).toBe(200);
+      expect(resp.body).toEqual({
+        'company': {
+          "handle": company1.handle,
+          "name": "COMPANY!",
+          "num_employees": company1.num_employees,
+          "description": company1.description,
+          "logo_url": company1.logo_url
+        }
+      });
+    });
+
+    test("will not update company that does not exist", async function () {
+      let resp = await request(app)
+        .patch(`/companies/none-here`)
+        .send({ "name": "COMPANY!" });
+
+      expect(resp.statusCode).toBe(404);
+    });
+  });
+
+  afterAll(function () {
+    db.end();
+  });
 
 });
--- models/company.js
+++ models/company.js
@@ -1,38 +1,68 @@
-const db = require('../db')
+const db = require('../db');
+const sqlForPartialUpdate = require('../helpers/partialUpdate');
 
 class Company {
 
-    static async findAll(search, min, max) {
-        let filters = [];
-        if (search !== undefined) {
-            filters.push(`name LIKE '%${search}%'`);
-        }
-        if (min !== undefined) {
-            filters.push(`num_employees >= ${min}`);
-        }
-        if (max !== undefined) {
-            filters.push(`num_employees >= ${max}`);
-        }
-
-        let companyResp
-
-        if (filters.length > 0) {
-            let queryString = `SELECT handle, name FROM companies WHERE ` + filters.join("AND ")
-            console.log(queryString)
-            companyResp = await db.query(queryString);
-        } else {
-            companyResp = await db.query(`SELECT handle, name FROM companies`);
-        }
-
-        return companyResp.rows;
-
+  static async findAll(search, min, max) {
+    let filters = [];
+    if (search !== undefined) {
+      filters.push(`name LIKE '%${search}%'`);
+    }
+    if (min !== undefined) {
+      filters.push(`num_employees >= ${min}`);
+    }
+    if (max !== undefined) {
+      filters.push(`num_employees >= ${max}`);
     }
 
-    static async create(data) {
-        const result = await db.query(`INSERT INTO companies (handle, name, num_employees, description, logo_url) VALUES ($1, $2, $3, $4, $5) RETURNING handle, name, num_employees, description, logo_url`, [data.handle, data.name, data.num_employees, data.description, data.logo_url]);
+    let companyResp;
 
-        return result.rows[0];
+    if (filters.length > 0) {
+      let queryString = `SELECT handle, name FROM companies WHERE ` + filters.join("AND ")
+      companyResp = await db.query(queryString);
+    } else {
+      companyResp = await db.query(`SELECT handle, name FROM companies`);
     }
+
+    return companyResp.rows;
+
+  }
+
+  static async create(data) {
+    const result = await db.query(
+      `INSERT INTO companies (handle, name, num_employees, description, logo_url) 
+             VALUES ($1, $2, $3, $4, $5) 
+             RETURNING handle, name, num_employees, description, logo_url`,
+      [data.handle, data.name, data.num_employees, data.description, data.logo_url]);
+
+    return result.rows[0];
+  }
+
+  static async findOne(handle){
+    const results = await db.query(
+      `SELECT handle, name, num_employees, description, logo_url
+      FROM companies
+      WHERE handle=$1`,
+      [handle]
+    );
+
+    if (results.rows.length === 0){
+      throw {message: `There is no company with the handle, ${handle}`, status: 404}
+    }
+
+    return results.rows[0];
+  }
+
+  static async update(handle, body){
+    const updated = sqlForPartialUpdate("companies", body, "handle", handle);
+    const results = await db.query(updated.query, updated.values);
+
+    if (results.rows.length === 0){
+      throw {message: `There is no company with the handle, ${handle}`, status: 404}
+    }
+
+    return results.rows[0];
+  }
 
 }
 
