b272477e452c48f43bf9a8c186ce364cf78149a5
jessicaelee <jessica.e.lee.93@gmail.com>
2020-02-19 10:07:29 -0800

    partialupdate unittest

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,16 @@
+const sqlForPartialUpdate = require('../../helpers/partialUpdate');
+const request = require('supertest');
+const app = require('../../app');
+
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
-      function () {
+    function () {
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+      let result = sqlForPartialUpdate("companies", { 'num_employees': 5 }, "handle", "handleID")
 
-  });
+      expect(result).toEqual({ query: `UPDATE companies SET num_employees=$1 WHERE handle=$2 RETURNING *`, values: [5, 'handleID'] });
+
+    });
 });
+
